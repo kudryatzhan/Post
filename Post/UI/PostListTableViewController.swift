69131feab265a829006545d13fc03db0bf154fd7
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-12-03 14:12:00 -0700

    Refresh Control, and network Activity Indicator implemented

--- Post/UI/PostListTableViewController.swift
+++ Post/UI/PostListTableViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class PostListTableViewController: UITableViewController {
-    
+class PostListTableViewController: UITableViewController, PostControllerDelegate {
+
     var postController = PostController()
+    
+    func stringFromTimeInterval(interval: TimeInterval) -> String {
+        let interval = Int(interval)
+        let seconds = interval % 60
+        let minutes = (interval / 60) % 60
+        let hours = (interval / 3600)
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        postController.delegate = self
+        
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        setupRefreshControl()
+        
+        
     }
 
     // MARK: - Table view data source
@@ -31,54 +47,32 @@ class PostListTableViewController: UITableViewController {
 
         // Configure the cell...
         cell.textLabel?.text = post.username
-        cell.detailTextLabel?.text = "\(post.username) \(post.timeStamp)"
+        cell.detailTextLabel?.text = "\(post.username) \(stringFromTimeInterval(interval: post.timeStamp))"
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // PostControllerDelegate
+    func postsWereUpdated(posts: [Post], on postController: PostController) {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
-    */
+}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+extension PostListTableViewController {
+    
+    // refreshControl
+    func setupRefreshControl() {
+        let rc = UIRefreshControl()
+        tableView.refreshControl = rc
+        rc.addTarget(self, action: #selector(reloadPosts), for: .valueChanged)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    @objc func reloadPosts() {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        postController.fetchPosts()
+        tableView.refreshControl?.endRefreshing()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
